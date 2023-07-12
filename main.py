6f6c38784ba4636d91217faff90bd6314c197cbc
jessicaduque <69450467+jessicaduque@users.noreply.github.com>
2023-07-12 13:55:49 -0300

    Melhoria visual

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import random
 from datetime import datetime, timedelta
 
 import matplotlib.pyplot as plt
-#plt.style.use("seaborn")
+plt.style.use("seaborn")
 from collections import deque
 from  matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
@@ -27,7 +27,7 @@ def ConfigurarCamera():
     vid = cv2.VideoCapture(0)
   
     # Declare the width and height in variables
-    width, height = 440, 180
+    width, height = 585, 250
   
     # Set the width and height
     vid.set(cv2.CAP_PROP_FRAME_WIDTH, width)
@@ -36,7 +36,7 @@ def ConfigurarCamera():
     return vid
   
 # Função de abrir a câmera e mostrar no video_widget do app
-def Open_Camera(frameCount):
+def Open_Camera():
     # Captura do vídeo frame por frame
     _, frame = vid.read()
 
@@ -47,7 +47,7 @@ def Open_Camera(frameCount):
     captured_image = Image.fromarray(opencv_image)
 
     # Converção da imagem capturada para photoimage
-    photo_image = CTkImage(captured_image, size=(480,200))
+    photo_image = CTkImage(captured_image, size=(585,250))
 
     # Definindo o photoimage do label
     video_widget.photo_image = photo_image
@@ -56,21 +56,25 @@ def Open_Camera(frameCount):
     video_widget.configure(image=photo_image)
   
     # Repetiçãoo do mesmo processo apóos 10 milisegundos
-    video_widget.after(10, Open_Camera, frameCount)
+    video_widget.after(10, Open_Camera)
 
 def CriacaoGrafico():
     queueTempo = deque([], maxlen = 15)
     queueDados = deque([], maxlen = 15)
 
-    queueDados.append(0) 
+    queueDados.append(2) 
     queueTempo.append("0")
 
     # to run GUI event loop
     fig, ax = plt.subplots()
+    plt.gcf().autofmt_xdate()
     linha, = ax.plot(list(queueTempo), list(queueDados))
-    plt.title("Diâmetro do cascão", fontsize=20)
+    #plt.title("Diâmetro do cascão", fontsize=20)
     plt.xlabel("Horas")
-    plt.ylabel("Diâmetro")
+    plt.ylabel("Diâmetro [mm]")
+    fig.set_figwidth(11)
+    fig.set_figheight(4)
+    
 
     return fig, queueDados, queueTempo, linha
 
@@ -90,7 +94,7 @@ def PlotarGraficoData(queueDados, queueTempo):
     y.append(numData)
 
     # Atualização do range dos eixos x e y
-    plt.ylim(min(list(y)), max(list(y)))
+    plt.ylim(min(list(y)) - 2, max(list(y)) + 2)
     plt.xlim(list(x)[0], list(x)[-1])
 
     # Atualização dos dados do eixo x e y
@@ -124,39 +128,43 @@ frameBaixo = CTkFrame(frameCentral, fg_color='#f5f3ee')
 frameBaixo.grid(row=1, column=0, padx=10,  pady=10)
 
 # Criação dos frames da parte de cima
-frameVideo = CTkFrame(frameCima, width=500, height=250, fg_color="white", border_color="gray", border_width=2, corner_radius=15)
+frameVideo = CTkFrame(frameCima, width=605, height=270, fg_color="white", border_color="gray", border_width=2, corner_radius=15)
 frameVideo.grid(row=0, column=0, padx=10,  pady=5)
 # Para frame do vídeo não adaptar tamanho aos componentes dentro
 frameVideo.grid_propagate(False)
 
-frameAlertGraph = CTkFrame(frameCima, width=250, height=250, fg_color="white", border_color="gray", border_width=2, corner_radius=15)
+frameAlertGraph = CTkFrame(frameCima, width=285, height=270, fg_color="white", border_color="gray", border_width=2, corner_radius=15)
 frameAlertGraph.grid(row=0, column=1, padx=10,  pady=5)
 frameAlertGraph.grid_propagate(False)
 
 # Criação dos frames da parte de baixo
-frameDataGraph = CTkFrame(frameBaixo, width=760, height=220, fg_color="white", border_color="gray", border_width=2, corner_radius=15)
+frameDataGraph = CTkFrame(frameBaixo, width=900, height=330, fg_color="white", border_color="gray", border_width=2, corner_radius=15)
 frameDataGraph.grid(row=0, column=0, padx=10,  pady=5)
-
+frameDataGraph.grid_propagate(False)
 
 # Criar o label do texto do vídeo e colocar em cima dele
-video_text_label = CTkLabel(frameVideo, text="Imagem Segmentada", font=("Arial", 23))
-video_text_label.grid(row=0, pady=3, padx=10, sticky='W')
+#video_text_label = CTkLabel(frameVideo, text="Imagem Segmentada", font=("Arial", 23))
+#video_text_label.grid(row=0, pady=3, padx=20, sticky='W')
+#video_text_label.place(relx=.5, rely=.5, anchor="w", x=10)
 
 # Criar o label do vídeo e mostrar no app
 video_widget = CTkLabel(frameVideo, text="")
-video_widget.grid(row=1, pady=3, padx=10)
-
-# Inicializa variável para aleatorizar dados
-frameCount = int()
+#video_widget.grid(row=1, pady=3, padx=20)
+video_widget.place(relx=.5, rely=.5, anchor="center")
 
 #Função para abrir ativar câmera e encaixar ela no app
-Open_Camera(frameCount)
+Open_Camera()
 
 # Criação do gráfico e chamada da função para atualizá-la
 fig, queueDados, queueTempo, linha = CriacaoGrafico()
-canvas = FigureCanvasTkAgg(fig, app)
-canvas.get_tk_widget().grid(row=0, column=1)
+canvas = FigureCanvasTkAgg(fig, frameDataGraph)
+canvas.get_tk_widget().place(relx=.5, rely=.5, anchor="center")
 PlotarGraficoData(queueDados, queueTempo)
+
+# Criação do gráfico de calibre e chamada da função para atualizá-la
+coresGrafCal = ['#4dab6d', 'f6ee54', 'ee4d55']
+valores = [0, 8, 15, 30]
+#valor
 
 # Função para rodar o app
 app.mainloop()
